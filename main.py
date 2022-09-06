cf6c1dac8020c9ce373b1ddbac0625b959b25622
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-09-07 00:40:05 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+
+os.mkdir('./export')
+with open('hi.txt','w') as f: 
+    f.write('hi you who see this.')
+
+os.chdir('./export')
+
+subprocess.run([
+    'git',
+    'init',
+    '-q'
+])
+subprocess.run([
+    'git', 
+    'checkout', 
+    '-q', 
+    '-b', 
+    'hi'
+])
+tags = subprocess.run(['git','remote','-v'], capture_output=True).stdout.decode('utf-8').split('\n')
+add_or_not = 'origin' in tags
+subprocess.run(['git', 'config', 'user.name', 'actions-user'])
+subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://x-access-token:{os.getenv("github-token")}@github.com/{os.getenv("github-repository")}'])
+subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'hi'])
+subprocess.run(['git', 'add', '.'])
+subprocess.run(['git', 'commit', '-q', '-m', '🚀 boom burst the big bang'])
+subprocess.run(['git', 'tag', '-f', 'hi'])
+subprocess.run(['git', 'push', '-f', '-q', '--tags', 'origin', 'hi'])
