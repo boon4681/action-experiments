35fa878924eaf6513f60131d76d0d7dd70e709e8
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-09-07 20:50:57 +0700

    test 006

--- main.py
+++ main.py
@@ -17,16 +17,17 @@ subprocess.run([
     'checkout', 
     '-q', 
     '-b', 
-    'hi'
-])
-tags = subprocess.run(['git','remote','-v'], capture_output=True).stdout.decode('utf-8').split('\n')
-add_or_not = 'origin' in tags
+    'yolo'
+], capture_output=True)
 subprocess.run(['git', 'config', 'user.name', 'actions-user'])
 subprocess.run(['git', 'config', 'user.email', 'actions@github.com'])
+
+tags = subprocess.run(['git','remote','-v'], capture_output=True).stdout.decode('utf-8').split('\n')
+add_or_not = 'origin' in tags
 subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://x-access-token:{os.getenv("github-token")}@github.com/{os.getenv("github-repository")}'])
-subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'hi'])
-subprocess.run(['git', 'reset', '-q', '--hard', f'origin/hi'])
+subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'yolo'])
+subprocess.run(['git', 'reset', '-q', '--hard', f'origin/yolo'])
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '-q', '-m', '🚀 boom burst the big bang'])
-subprocess.run(['git', 'tag', '-f', 'hi'])
-subprocess.run(['git', 'push', '-f', '-q', '--tags', 'origin', 'hi'])
+subprocess.run(['git', 'tag', '-f', 'yolo'])
+subprocess.run(['git', 'push', '-q', '--tags', 'origin', 'yolo'])
