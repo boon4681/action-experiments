4bf31413228829881d23ef40e11ac7af28ffa2e7
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-09-09 18:48:47 +0700

    hmmm

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import subprocess
 import os
 
-os.makedirs('./export',exist_ok=True)
-os.chdir('./export')
+os.makedirs('./yield',exist_ok=True)
+os.chdir('./yield')
 
-with open('hi.txt','w') as f: 
-    f.write('hi you.')
+with open('yield.txt','w') as f: 
+    f.write('hell you git.')
 
 subprocess.run([
     'git',
@@ -17,21 +17,19 @@ subprocess.run([
     'checkout', 
     '-q', 
     '-b', 
-    'export'
+    'dumb'
 ], capture_output=True)
-# subprocess.run(['git', 'config', 'user.name', 'actions-user'])
-# subprocess.run(['git', 'config', 'user.email', 'actions@github.com'])
+subprocess.run(['git', 'config', 'user.name', 'actions-user'])
+subprocess.run(['git', 'config', 'user.email', 'actions@github.com'])
 
 tags = subprocess.run(['git','remote'], capture_output=True).stdout.decode('utf-8').split('\n')
 add_or_not = 'origin' in tags
-# subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://x-access-token:{os.getenv("github-token")}@github.com/{os.getenv("github-repository")}'])
-subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://github.com/boon4681/action-experiments.git'])
+subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://x-access-token:{os.getenv("github-token")}@github.com/{os.getenv("github-repository")}'])
+# subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://github.com/boon4681/action-experiments.git'])
 
-subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'export'])
-subprocess.run(['git', 'reset', '-q', '--hard', f'origin/export'])
-
+subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'yeah'])
 
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '-q', '-m', '🚀 boom burst the big bang'])
-subprocess.run(['git', 'tag', '-f', 'export'])
-subprocess.run(['git', 'push','-f','-q', '--tags', 'origin', 'export'])
+subprocess.run(['git', 'tag', '-f', 'yeah'])
+subprocess.run(['git', 'push','-f','-q', '--tags', 'origin'])
