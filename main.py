10b9c215a729903043bdda0f9add63a0b1cfd548
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-09-17 22:10:19 +0700

    test again

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ subprocess.run(['git', 'config', 'user.email', 'actions@github.com'])
 tags = subprocess.run(['git','remote'], capture_output=True).stdout.decode('utf-8').split('\n')
 add_or_not = 'origin' in tags
 subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://x-access-token:{os.getenv("github-token")}@github.com/{os.getenv("github-repository")}'])
-subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'yeah'])
+subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'dumb'])
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '-q', '-m', '🚀 boom burst the big bang'])
-subprocess.run(['git', 'tag', '-f', 'yeah'])
+subprocess.run(['git', 'tag', '-f', 'dumb'])
 subprocess.run(['git', 'push','-f','-q', '--tags', 'origin'])
 
